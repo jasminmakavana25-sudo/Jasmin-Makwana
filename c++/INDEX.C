80f88ce5e90a7debe77f8e2450dd7d842a2d4040
princegabani-01 <princegabani358@gmail.com>
2026-08-24 23:18:42 +0530

    MSG

---
+++ c++/INDEX.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+
+	clrscr();
+	printf("Hello!! CDMI");
+	printf("\t\tSpace word");
+	printf("\n\nnew line");
+
+	printf("\n\nName \t\t:-\t Demo");
+	printf("\nSurname  \t:- \t Cdmi");
+	printf("\nEmail     \t:-\t ______________________________");
+	printf("\nnumber    \t:-\t box");
+	printf("\nAddress   \t:-\t ______________________________");
+	printf("\n	        \t ______________________________");
+	printf("\nUser Name\t :-\t derdevil");
+
+
+
+	return 0;
+}
