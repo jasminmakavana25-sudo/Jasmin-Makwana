b315efced60b27dbf48f102258ee3a5256c61a56
princegabani-01 <princegabani358@gmail.com>
2026-08-26 12:52:17 +0530

    MSG

---
+++ c++/VOID.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=10 ,b=20 ,c=1, derdevil=3200, get=123 tony=25;
+	clrscr();
+
+	printf("\n=%d",a);
+	printf("\n=%d",b);
+	printf("\n=%d",c);
+	printf("\n=%d",derdevil);
+	printf("\n=%d",get);
+	printf("\n=%d",tony);
+	getch();
+}
+
