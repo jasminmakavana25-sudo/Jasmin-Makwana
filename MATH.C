bc2f8ef7fcf81f9d52d86be90873f6e225ed9af8
princegabani-01 <princegabani358@gmail.com>
2026-08-27 12:29:46 +0530

    MSG

---
+++ MATH.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=82,b=222,g=30,sum,minus,multi,d,m;
+	clrscr();
+
+       //	sum = apple+boom;
+      //	printf("\nSum %d",sum);
+
+	minus = a-b;
+	printf("\nminus  %d",minus);
+
+	multi = a*b;
+	printf ("\nMulti  %d",multi);
+
+	d =b/a;
+	printf("\nD %d", d);
+
+	m =b%a%g;
+	printf("\nm %d",m);
+
+	getch();
+
+}
